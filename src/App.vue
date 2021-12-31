7c48ef56cdd555387e2a452dd18f9264e662f4b5
Eden Keshet <edenkeshet@gmail.com>
2021-12-31 22:09:39 +0200

    initial state

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainScreen loginUser="eden"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MainScreen from "@/screens/MainScreen";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainScreen
   }
 }
 </script>
